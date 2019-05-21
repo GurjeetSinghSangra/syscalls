33b7797285e0f7c54a87012963134b6f655703c4
Gurjeet Sangra <sardargurjeet95@gmail.com>
2019-05-21 18:41:48 +0200

    Added files header

---
+++ clientReq-server/inc/keygenerator.h
@@ -0,0 +1,6 @@
+#ifndef _KEYGENERATOR_HH
+#define _KEYGENERATOR_HH
+
+long generateKey(long requestNumber, short service);
+
+#endif
---
+++ clientReq-server/inc/keymanager.h
@@ -0,0 +1,7 @@
+#ifndef _KEYMANAGER_HH
+#define _KEYMANAGER_HH
+
+int keymanager();
+long insertKey(long key, char userCode[]);
+
+#endif
